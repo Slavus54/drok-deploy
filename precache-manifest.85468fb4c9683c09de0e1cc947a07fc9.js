213ae9a54e8deef3514b585663301d730dec0e24
Slavus54 <x.miros@yandex.ru>
2020-05-26 21:48:27 +0700

    Updates

---
+++ precache-manifest.85468fb4c9683c09de0e1cc947a07fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d66c0928c2a46d85839e5a29315d0a",
+    "url": "/drok-deploy/index.html"
+  },
+  {
+    "revision": "8cf15e536a5fdb387309",
+    "url": "/drok-deploy/static/css/main.b9aeb2f8.chunk.css"
+  },
+  {
+    "revision": "4625dfb5ad5f10ff3e36",
+    "url": "/drok-deploy/static/js/2.a8314157.chunk.js"
+  },
+  {
+    "revision": "8cf15e536a5fdb387309",
+    "url": "/drok-deploy/static/js/main.7a724612.chunk.js"
+  },
+  {
+    "revision": "1eca269d095a31a30f28",
+    "url": "/drok-deploy/static/js/runtime-main.bed8fdc8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/drok-deploy/static/media/logo.25bf045c.svg"
+  }
+]);
